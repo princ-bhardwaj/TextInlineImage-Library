667b0c3a093dd92846785469bfc6b75b1d44edd6
Prince Bhardwaj <prince.bhardwaj@oodlestechnologies.com>
2019-01-27 17:35:26 +0530

    Package name change

--- app/src/main/java/com/app/printexample/textinlineimagesample/MainActivity.java
+++ app/src/main/java/com/app/printexample/textinlineimagesample/MainActivity.java
@@ -1,11 +1,7 @@
 package com.app.printexample.textinlineimagesample;
 
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import com.app.printexample.textinlineimage.TextInlineImage;
 
 public class MainActivity extends AppCompatActivity {
 
